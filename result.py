f0005d8304b0f58a2e0c61b9922e5bdaa9ced27f
tsubasa-km <mc0627tsubasa283@gmail.com>
2025-01-21 11:45:05 +0900

    戦績の読み取りを簡略化

--- result.py
+++ result.py
@@ -1,5 +1,4 @@
 from dotenv import load_dotenv
-import matplotlib.pyplot as plt
 import pyocr.builders
 from glob import glob
 from PIL import Image
@@ -38,32 +37,26 @@ def result_ocr(img: Image.Image):
 def format_result(txt: str):
     lines = txt.replace(" ","").split("\n")
     pattern = re.compile(r"^\d*px\d*(<\d*>)?x\d*x\d*")
+    value = []
+    for line in lines:
+        if pattern.match(line):
+            splat, ka, d, sp = line.replace("p","").split("x")
+            k, a = ka.replace(">","").split("<") if "<" in ka else (ka, "0")
+            value.append((int(k), int(a), int(d), int(sp), int(splat)))
+    return value
 
 
 def crop_result(img: np.ndarray):
     img = img[300:-350,-280:-25]
     _, img = cv2.threshold(img, 80, 255, cv2.THRESH_BINARY)
-    # return [
-    #     img[83:110, :],
-    #     img[181:208, :],
-    #     img[278:305, :],
-    #     img[376:403, :],
-    #     img[530:557, :],
-    #     img[627:654, :],
-    #     img[725:752, :],
-    #     img[822:849, :],
-    # ]
     return img
 
 
 if __name__ == '__main__':
-    img = cv2.imread(SCREENSHOT_DIR+"/1.jpg")
-    if img is None:
-        raise Exception("Image not found")
-    results = crop_result(img)
-    # for result in results:
-    #     result = Image.fromarray(result)
-    #     txt = result_ocr(result)
-    #     print(format_result(txt))
-    txt = result_ocr(Image.fromarray(results))
-    print(format_result(txt))
+    for i in range(1,6):
+        img = cv2.imread(f"{SCREENSHOT_DIR}/{i}.jpg")
+        if img is None:
+            raise Exception("Image not found")
+        results = crop_result(img)
+        txt = result_ocr(Image.fromarray(results))
+        print(format_result(txt))
