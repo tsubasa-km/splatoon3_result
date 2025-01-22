a50f230d83391a47bfde904e2707f9237ea5a71f
tsubasa-km <mc0627tsubasa283@gmail.com>
2025-01-22 11:03:40 +0900

    ルール処理のための新しいモジュールを追加し、画像の特徴を抽出・比較する機能を共通化

---
+++ rule.py
@@ -0,0 +1,33 @@
+import cv2
+
+from image import get_icon_features, compare_icons, calculate_overall_similarity
+
+
+RULE_PATHS = {
+    "zone": "./resource/rules/zone.jpg",
+    "tower": "./resource/rules/tower.jpg",
+    "rainmaker": "./resource/rules/rainmaker.jpg",
+    "blitz": "./resource/rules/blitz.jpg",
+}
+
+
+def crop_rule(image: cv2.Mat):
+    return image[50:130, 200:540]
+
+
+def get_rule_name(image):
+    image = crop_rule(image)
+    for name, path in RULE_PATHS.items():
+        rule = cv2.imread(path)
+        features = get_icon_features(image), get_icon_features(rule)
+        shape_similarity, color_similarity = compare_icons(*features)
+        overall_similarity = calculate_overall_similarity(
+            shape_similarity, color_similarity)
+        if overall_similarity > 0.9:
+            return name
+    return "unknown"
+
+
+if __name__ == "__main__":
+    image = cv2.imread("./screenshots/1.jpg")
+    print(get_rule_name(image))
--- weapons.py
+++ weapons.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+from image import get_icon_features, compare_icons, calculate_overall_similarity
+
 __circles = None
 
 orb = cv2.ORB_create(
@@ -47,74 +49,6 @@ def crop_icons(img, circles):
         crop = img[y - r:y + r, x - r:x + r]
         crops.append(crop)
     return crops
-
-
-def extract_features(image):
-    """形状特徴量（ORB）を抽出"""
-    keypoints, descriptors = orb.detectAndCompute(image, None)
-    return keypoints, descriptors
-
-
-def extract_color_histogram(image):
-    """色ヒストグラムを抽出"""
-    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    hist = cv2.calcHist([hsv_image], [0, 1], None, [50, 60], [
-                        0, 180, 0, 256])  # H:50, S:60
-    cv2.normalize(hist, hist)
-    return hist
-
-
-def display_results(image, keypoints, hist):
-    """抽出結果を表示"""
-    keypoint_image = cv2.drawKeypoints(
-        image, keypoints, None, color=(0, 255, 0))
-
-    plt.figure(figsize=(10, 5))
-    plt.subplot(1, 2, 1)
-    plt.imshow(cv2.cvtColor(keypoint_image, cv2.COLOR_BGR2RGB))
-    plt.title("Keypoints")
-    plt.axis("off")
-
-    plt.subplot(1, 2, 2)
-    plt.title("Color Histogram (Hue-Saturation)")
-    plt.xlabel("Bins")
-    plt.ylabel("Frequency")
-    plt.imshow(hist, interpolation='nearest', aspect='auto', cmap='viridis')
-    plt.colorbar(label="Frequency")
-    plt.show()
-
-
-def get_icon_features(image):
-    """画像を読み込み、特徴量を抽出して表示"""
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    keypoints, descriptors = extract_features(gray_image)
-    color_hist = extract_color_histogram(image)
-    # display_results(image, keypoints, color_hist)
-    return keypoints, descriptors, color_hist
-
-
-def compare_icons(features_a, features_b):
-    """2つの画像の特徴量を比較し、形状と色の類似度を計算"""
-    keypoints_a, descriptors_a, color_hist_a = features_a
-    keypoints_b, descriptors_b, color_hist_b = features_b
-    if descriptors_a is not None and descriptors_b is not None:
-        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-        matches = bf.match(descriptors_a, descriptors_b)
-        shape_similarity = len(matches) / \
-            max(len(keypoints_a), len(keypoints_b))
-    else:
-        shape_similarity = 0.0
-    color_similarity = cv2.compareHist(
-        color_hist_a, color_hist_b, cv2.HISTCMP_CORREL)
-    return shape_similarity, color_similarity
-
-
-def calculate_overall_similarity(shape_similarity, color_similarity, weight_shape=0.5, weight_color=0.5):
-    """形状と色の類似度を統合スコアとして計算"""
-    normalized_color_similarity = (color_similarity + 1) / 2
-    overall_similarity = (weight_shape * shape_similarity +
-                          weight_color * normalized_color_similarity)
-    return overall_similarity
 
 
 if __name__ == "__main__":
