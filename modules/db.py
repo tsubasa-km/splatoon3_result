2ec65491c1d42dd758107996ae5a5731edb3cb1f
tsubasa-km <mc0627tsubasa283@gmail.com>
2025-01-29 08:56:33 +0900

    データベースの初期化条件を修正し、存在しない場合のみテーブルを作成するように変更。インポートパスを整理。

--- modules/db.py
+++ modules/db.py
@@ -1,13 +1,20 @@
+from models.weapon import Weapon
+from models.team import Team
+from models.sub_weapon import SubWeapon
+from models.stage import Stage
+from models.special_weapon import SpecialWeapon
+from models.player import Player
+from models.game import Game
+from models.base import Base
 import sys
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
-from models.base import Base
-from models import game, player, special_weapon, stage, sub_weapon, team, weapon
 
 engine = create_engine("sqlite:///database.db", echo=True)
 
-Base.metadata.create_all(engine)
+if not os.path.exists("database.db"):
+    Base.metadata.create_all(engine)
